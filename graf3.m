3621f173561a8f6734a6f26515dc7eb064577434
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2021-10-28 13:18:52 -0500

    Add files via upload

---
+++ graf3.m
@@ -0,0 +1,16 @@
+% Octave Script
+% Title         :Funcion real de la variable real
+% Description   :Script del ejercicio 3
+% Author        :Rafael Miranda Jimenez
+% Date          :20211028
+% Version       :1
+% Usage         :octave> /path/graf3
+% Notes         :Requiere aplicación octave y usar linea de comandos
+clear
+syms t
+% dominio de la función 
+  r=-50:0.1:50;
+% regla de correspondencia (función)
+  ht= (t) / (2-t)
+% plotear función
+ezplot(ht)  
